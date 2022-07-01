4ce510446b1269785f85be68717e8cc2e3ab2c58
JohnathanHarbertHolder <holderjohnathanh@gmail.com>
2022-06-30 20:04:19 -0700

    Holder C Programming

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,4 @@
-#!/bin/bash
+#include <stdio.h>
 
 /**
  * main - Entry point
@@ -7,6 +7,6 @@
  */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
