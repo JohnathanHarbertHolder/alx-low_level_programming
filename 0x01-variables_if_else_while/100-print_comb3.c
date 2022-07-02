133b04367c656d560c5a12991a5c365bfeb7355b
JohnathanHarbertHolder <holderjohnathanh@gmail.com>
2022-07-01 20:59:16 -0700

    Holder ALX

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,25 +1,28 @@
 #include <stdio.h>
-#include <stdlib.h>
 /**
- * main - main block
- * Description: prints all single digit numbers of base 10
- * starting from 0, followed by a new line.
- * Return: 0
+ * main - output all single digit number of base 10
+ *
+ * Return: 0 if successful
  */
 int main(void)
 {
-int i = 0;
-int j = 0;
-for (i = 0; i < 10; i++)
-{
-for (j = i + 1; j < 10; j++)
-{
-putchar(48 + i);
-putchar(48 + j);
-putchar(',');
-putchar(' ');
-}
-}
-putchar ('\n');
-return (0);
+	int var;
+	for (var = 0; var < 9; var++)
+	{
+		int ii;
+		for (ii = var + 1; ii < 10; ii++)
+		{
+			putchar((var % 10) + '0');
+			putchar((ii % 10) + '0');
+			if (var != 8)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			else
+				continue;
+		}
+	}
+	putchar('\n');
+	return (0);
 }
