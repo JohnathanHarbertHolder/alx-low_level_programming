1f2fa49c8fb34dde21aa305111a52a9fdf58268a
JohnathanHarbertHolder <holderjohnathanh@gmail.com>
2022-07-07 20:03:54 -0700

    HOLDER ALX

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * print_number - prints an integer
  * @n: integer to be printed
@@ -7,7 +6,6 @@
 void print_number(int n)
 {
 	unsigned int n1;
-
 	if (n < 0)
 	{
 		n1 = -n;
@@ -16,11 +14,9 @@ void print_number(int n)
 	{
 		n1 = n;
 	}
-
 	if (n1 / 10)
 	{
 		print_number(n1 / 10);
 	}
-
 	_putchar((n1 % 10) + '0');
 }
