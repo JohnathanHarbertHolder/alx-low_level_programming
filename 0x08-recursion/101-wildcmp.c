65325cb2bb52e739541437695c30f422ba645fe0
JohnathanHarbertHolder <holderjohnathanh@gmail.com>
2022-07-19 21:24:18 -0700

    last commit

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,27 +1,17 @@
-#include "main.h"
+#include <stdio.h>
+
+int wildcmp(char *, char *);
 
 /**
- * wildcmp - Funtion
+ * main - check the code for Holberton School students.
  *
- * @s1: Variable
- * @s2: Variable
- *
- * Return: Int
+ * Return: Always 0.
  */
-int wildcmp(char *s1, char *s2)
+int main(void)
 {
+	int r;
 
-	if (*s1 == *s2 && *s1 != '\0' && *s2 != '\0')
-		return (wildcmp(s1 + 1, s2 + 1));
-	if (*s1 == '\0' && *s2 == '\0')
-		return (1);
-
-	if (*s2 == '*')
-	{
-		if (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2))
-			return (1);
-		if (*(s1 + 1) != '\0' && *s2 == '\0')
-			return (0);
-	}
+	r = wildcmp("main.c", "ma*********************************************************d");
+	printf("%d\n", r);
 	return (0);
 }
