76de4afd8ea864c040e6bdb8fa3f5d25aa4d87c2
JohnathanHarbertHolder <holderjohnathanh@gmail.com>
2022-10-06 16:31:33 -0700

    ALX

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -1,13 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdlib.h>
-
-int add(int a, int b);
-int sub(int a, int b);
-int mul(int a, int b);
-int div(int a, int b);
-int mod(int a, int b);
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -28,5 +21,4 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
-
-#endif
+#endif /* MAIN_H */
